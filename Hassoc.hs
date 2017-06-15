64fa529ce6441d23b38400975de2bd052fca2bab
= <=>
2017-06-16 00:55:23 +0200

    File IO

--- Hassoc.hs
+++ Hassoc.hs
@@ -10,3 +10,33 @@ Description:
 3. generates PHP Associative Array
 4. writes the array to file
 -}
+
+module Hassoc
+( 
+    main
+) 
+where
+
+import System.IO
+import Data.List
+
+-----------------------------------------------------------
+-- Constants
+-----------------------------------------------------------
+
+inputFile :: FilePath
+inputFile = "input.txt"
+
+outputFile :: FilePath
+outputFile = "output.txt"
+
+-----------------------------------------------------------
+-- Functions
+-----------------------------------------------------------
+
+main :: IO ()
+main = do
+
+    inputContents <- readFile inputFile
+    let properties = words inputContents
+    print properties
